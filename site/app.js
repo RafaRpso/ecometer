1aed2121e714620bfdec71920e3ff36d3e64a7d9
Rafael <54147384+RafaRpso@users.noreply.github.com>
2022-06-07 20:17:20 -0300

    Update app.js

--- site/app.js
+++ site/app.js
@@ -4,7 +4,7 @@
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = process.env.AMBIENTE_PROCESSO = "desenvolvimento" ? 3333 : 8000;
+var PORTA = process.env.AMBIENTE_PROCESSO = "producao" ? 3333 : 8000;
 
 var app = express();
 
